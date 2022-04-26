80864b17ff730f9d3541a1971a9e80c08e5d6355
Stephane Duval <stephane.duval@isi-mtl.com>
2022-04-26 16:10:49 -0400

    retour au programme d'origine

--- Eclipse/hello world/src/Principale.java
+++ Eclipse/hello world/src/Principale.java
@@ -1,7 +1,6 @@
 
 public class Principale {
 	public static void main(String[] args) {
-		System.out.println("Bonjour tout le monde !");
-		System.out.println("Je suis un super programme !");
+		System.out.println("Hello world!");
 	}
 }
