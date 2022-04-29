2e13ce6aeabfe39473c5f73a70b2149a43cd0c0c
Stephane Duval <stephane.duval@isi-mtl.com>
2022-04-29 11:16:17 -0400

    illustration Kahoot sur les wrappers

--- Eclipse/EX 04 - exercice A/src/premier/Toto.java
+++ Eclipse/EX 04 - exercice A/src/premier/Toto.java
@@ -4,9 +4,9 @@ class Toto {
 	void f() {
 		Base p = new Base();
 		p.packageMember = 90;
-		//p.privateMember = 12;	//	impossible
+		// p.privateMember = 12; // impossible
 		p.protectedMember = 123;
 		p.publicMember = 123;
 	}
-	
+
 }
