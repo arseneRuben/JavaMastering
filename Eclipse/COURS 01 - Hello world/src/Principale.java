6ea4b03eb37be82a216de156bebb9f7071f09e79
Stephane Duval <stephane.duval@isi-mtl.com>
2022-08-11 10:18:58 -0400

    test singleton

--- Eclipse/COURS 01 - Hello world/src/Principale.java
+++ Eclipse/COURS 01 - Hello world/src/Principale.java
@@ -1,6 +1,27 @@
+class Singleton {
+	private static Singleton instance = null;
+
+	private Singleton() {
+	}
+
+	static public Singleton getInstance() {
+		if (Singleton.instance == null) {
+			Singleton.instance = new Singleton();
+		}
+		return Singleton.instance;
+	}
+}
 
 public class Principale {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+	static int help() {
+		System.out.println("dans help de Principale: OMG");
+		return 666;
 	}
+
+	public static void main(String[] args) {
+		System.out.println("dans main: Hello world!");
+	}
+
+	public static final int x = Principale.help();
 }
